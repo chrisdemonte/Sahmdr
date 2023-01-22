46b752f3278aa662248c7b41890dcbcbaa5f1882
chrisdemonte <46972602+chrisdemonte@users.noreply.github.com>
2023-01-21 21:31:45 -0500

    AWS Deployment
    Preparing for deployment on S3 aws hosting

--- samhdr-app/src/index.js
+++ samhdr-app/src/index.js
@@ -3,13 +3,44 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import io from 'socket.io-client'
+import io from 'socket.io-client';
+import pem from './ssl/samhdr-server-key.pem'
+
+//const filereader = new FileReader()
+//const key = filereader.readAsText('./ssl/samhdr-server-key.txt')
+
 
 var socket 
 
+/*
+fetch(pem).then((r) => r.text()).then(
+  text => {
+    try {
+      socket = io('https://localhost:2020',{
+      key: text})
+      //  cert: reader.readAsText("../ssl/samhdr-cert.pem"),
+      //  ca: [
+      //    reader.readAsText("../ssl/samhdr-server-cert.pem")
+      //  ]
+    // });
+      //socket = io('https://54.159.107.122:443')
+    }
+    catch (error){
+      
+    }
+  }
+  )*/
+
 try {
-  //socket = io('http://localhost:2020')
-  socket = io('http://54.91.207.24:80')
+  socket = io('http://localhost:2020')//,{
+ //  key: fs.readFileSync('../ssl/samhdr-server-key')})
+  //  cert: reader.readAsText("../ssl/samhdr-cert.pem"),
+  //  ca: [
+  //    reader.readAsText("../ssl/samhdr-server-cert.pem")
+  //  ]
+ // });
+  //socket = io('https://52.21.208.75')
+  //socket = io('samhdr-load-balancer-716332415.us-east-1.elb.amazonaws.com:2020')
 }
 catch (error){
   
